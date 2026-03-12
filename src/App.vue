9538a6ec002ac9b87cab4ec623295f1a189ab9e9
dylcaton <dylanpcaton@gmail.com>
2026-03-12 00:18:38 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ShoppingCart class="shopping-cart"></ShoppingCart>
+  </div>
+</template>
+
+<script setup>
+import ShoppingCart from './components/ShoppingCart.vue';
+
+</script>
+
+<style scoped>
+.shopping-cart {
+            font-family: 'Arial', sans-serif;
+            background-color: #f8f8f8;
+            margin: 0;
+            padding: 0;
+        }
+
+</style>
---
+++ src/components/CartTitle.vue
@@ -0,0 +1,11 @@
+<template>
+         <h1>Harry's Shopping Cart</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
