bcd12d0f985c2a91fbcc0aa6e9910048aeb106ba
dylcaton <dylanpcaton@gmail.com>
2026-03-13 09:46:38 -0500

    props

--- src/components/CartTitle.vue
+++ src/components/CartTitle.vue
@@ -1,11 +1,17 @@
 <template>
-         <h1>Harry's Shopping Cart</h1>
+         <h1>{{username}}'s Shopping Cart</h1>
 </template>
 
 <script setup>
+defineProps(['username'])
 
 </script>
 
 <style scoped>
+h1 {
+    padding: 20px;
+    max-width: 1200px;
+    margin: auto;
+}
 
 </style>
